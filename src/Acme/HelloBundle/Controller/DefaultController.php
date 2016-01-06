78ac312fe50a9795878de41fbd19e7b3539a67dd
Ricardo Ovando <zine56@hotmail.com>
2016-01-06 17:30:11 -0300

    primera carga

---
+++ src/Acme/HelloBundle/Controller/DefaultController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Acme\HelloBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Acme\HelloBundle\Entity\Blog;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('AcmeHelloBundle:Default:index.html.twig');
+    }
+    public function homeAction()
+    {
+    	return $this->render('AcmeHelloBundle:Default:home.html.twig');
+    }
+    public function bienvenidoAction()
+    {
+    	return $this->render ('AcmeHelloBundle:Default:bienvenido.html.twig');
+    }
+    public function testAction()
+    {
+        return $this->render('AcmeHelloBundle:vista1:prueba.html.twig');
+    }
+    public function postAction(Request $aux)
+    {
+      $blog = "";       
+      $titulo = "";
+      $paso = 0;
+
+      if(  $aux->isMethod('POST'))
+      {
+
+        if(empty($titulo) && empty($blog) ){
+
+        }
+        $titulo=$aux->request->get('titulo');
+        $blog=$aux->request->get('blog');
+
+        $post = new Blog();
+
+        $post->setTitulo($titulo);
+
+        $post->setCuerpo($blog);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($post);
+        $em->flush();
+        $paso =1;
+
+      }
+
+        
+        return $this->render('AcmeHelloBundle:blog:postear.html.twig',array('blog'=>$blog,'esteselpao'=>$paso));
+    }
+    public function inicioAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $aux= $em->getRepository('AcmeHelloBundle:Blog')->findAll();
+        
+        //var_dump($aux);
+
+        return $this->render('AcmeHelloBundle:blog:index.html.twig',array('blogs'=>$aux));
+    }
+    public function buscaridAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $xok= $em->getRepository('AcmeHelloBundle:Blog')->find($id);
+
+        return $this->render('AcmeHelloBundle:blog:id.html.twig', array('lol' => $xok ));
+    }
+   
+    public function eliminarporidAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $hue= $em->getRepository('AcmeHelloBundle:Blog')->find($id);
+        $em->remove($hue);
+        $em->flush();
+
+        $aux= $em->getRepository('AcmeHelloBundle:Blog')->findAll();
+
+        return $this->render('AcmeHelloBundle:blog:eliminar.html.twig', array('delete' => $hue,'lista'=>$aux));
+    }
+}
---
+++ src/Acme/HelloBundle/Entity/Blog.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Acme\HelloBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Blog
+ *
+ * @ORM\Table(name="blog")
+ * @ORM\Entity
+ */
+class Blog
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="titulo", type="string", length=20, nullable=true)
+     */
+    private $titulo;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="cuerpo", type="string", length=100, nullable=true)
+     */
+    private $cuerpo;
+
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set titulo
+     *
+     * @param string $titulo
+     *
+     * @return Blog
+     */
+    public function setTitulo($titulo)
+    {
+        $this->titulo = $titulo;
+
+        return $this;
+    }
+
+    /**
+     * Get titulo
+     *
+     * @return string
+     */
+    public function getTitulo()
+    {
+        return $this->titulo;
+    }
+
+    /**
+     * Set cuerpo
+     *
+     * @param string $cuerpo
+     *
+     * @return Blog
+     */
+    public function setCuerpo($cuerpo)
+    {
+        $this->cuerpo = $cuerpo;
+
+        return $this;
+    }
+
+    /**
+     * Get cuerpo
+     *
+     * @return string
+     */
+    public function getCuerpo()
+    {
+        return $this->cuerpo;
+    }
+}
